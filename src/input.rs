53bb8f09d9f68c0bedc1006d741f1cd859af1649
arjpeg <araoudupi@gmail.com>
2023-06-04 17:13:33 -0500

    add about with clap

--- src/input.rs
+++ src/input.rs
@@ -1,6 +1,11 @@
 use clap::Parser;
 
 #[derive(Debug, Parser)]
+#[command(
+    author = "arjpeg",
+    version = "0.1.0",
+    about = "A simple CLI tool to recursively search for a pattern in a file or directory."
+)]
 pub struct Cli {
     /// The pattern string to search for
     pub pattern: String,
